b497dd75725c8315ad29c3780cc926fd899aff5c
Raihan Talukdar <ardour_margate9s@icloud.com>
2025-12-07 20:00:25 +0000

    Initial Commit

---
+++ WalkingWR/WalkingWRApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalkingWRApp.swift
+//  WalkingWR
+//
+//  Created by Raihan Talukdar on 07/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct WalkingWRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
